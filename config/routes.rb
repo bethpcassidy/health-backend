f73911dfcf62f13d9d9d3e139e1186e356049d3e
beth <bethcassdy@gmail.com>
2023-10-08 14:14:55 -0400

    login foundation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :appointments, format: "json"
   resources :patients, format: "json"
   resources :doctors, format: "json"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
