34d11173b7d55c4016d4f5fa28ded52073a22b6c
beth <bethcassdy@gmail.com>
2023-10-19 20:20:49 -0400

    sessions fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,38 @@
 class SessionsController < ApplicationController
-  def new
-    render template: "sessions/new"
-  end
- 
-  def create
+
+
+
+  def create 
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to "/"
+      jwt = JWT.encode(
+        {
+          user_id: user.id,
+          exp: 24.hours.from_now.to_i #expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base),"HS256"
+      )
+      render json: { jwt: jwt, email: user.email, user_id: user.id }, status:created
     else
-      redirect_to "/login"
+      render json: {}, status: :unauthorized
     end
   end
+  # def new
+  #   render template: "sessions/new"
+  # end
  
-  def destroy
-    session[:user_id] = nil
-    redirect_to "/login"
-  end
+  # def create
+  #   user = User.find_by(email: params[:email])
+  #   if user && user.authenticate(params[:password])
+  #     session[:user_id] = user.id
+  #     redirect_to "/"
+  #   else
+  #     redirect_to "/login"
+  #   end
+  # end
+ 
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to "/login"
+  # end
 end
